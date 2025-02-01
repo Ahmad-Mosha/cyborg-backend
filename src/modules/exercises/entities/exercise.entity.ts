1bef520c5464f2d7537b671d52edb48ca6e5dce0
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-02-01 22:09:40 +0200

    implement exercise module

---
+++ src/modules/exercises/entities/exercise.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('exercises')
+export class Exercise {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  bodyPart: string;
+
+  @Column()
+  equipment: string;
+
+  @Column()
+  gifUrl: string;
+
+  @Column()
+  target: string;
+
+  @Column('simple-array')
+  secondaryMuscles: string[];
+
+  @Column('simple-array')
+  instructions: string[];
+}
