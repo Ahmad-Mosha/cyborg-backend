ed5b8355659d305b03e4e32df9ca25ba764c2063
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-02-01 19:56:02 +0200

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  });
+  await app.listen(3001);
 }
 bootstrap();
