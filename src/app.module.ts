1ae16f2907d6fa52ec2cc65236cd2c0970afe1ea
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-02-19 15:58:35 +0200

    fix food module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-import { FoodModule } from './modules/food/food.module';
 import { ExercisesModule } from './modules/exercises/exercises.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { databaseConfig } from './config/database.config';
@@ -17,11 +16,9 @@ import { RecipeModule } from './modules/recipe/recipe.module';
     TypeOrmModule.forRoot(databaseConfig),
     AuthModule,
     UsersModule,
-    FoodModule,
     ExercisesModule,
     ChatModule,
-    RecipeModule
-    
+    RecipeModule,
   ],
   controllers: [],
   providers: [],
