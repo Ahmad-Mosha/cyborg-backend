035d21006ef45658c7dc437a6b4305c2c632c560
Amira <amirahussein632@gmail.com>
2025-02-20 02:04:55 +0200

    Updated DTOs: refined create and update structures for posts and comments

--- src/modules/community/dto/create-post.dto.ts
+++ src/modules/community/dto/create-post.dto.ts
@@ -1,22 +1,32 @@
-// import { IsString, IsEnum, IsOptional, IsArray } from 'class-validator';
-// import { PostType } from '../entities/post.entity';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsEnum, IsOptional, IsArray, Length, IsNotEmpty } from 'class-validator';
+import { PostType } from '../entities/post.entity';
 
-// export class CreatePostDto {
-//   @IsString()
-//   title: string;
+export class CreatePostDto {
+  @ApiProperty({ description: 'Post title', minLength: 3, maxLength: 100 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 100)
+  title: string;
 
-//   @IsString()
-//   content: string;
+  @ApiProperty({ description: 'Post content', minLength: 10 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 5000)
+  content: string;
 
-//   @IsEnum(PostType)
-//   @IsOptional()
-//   type?: PostType;
+  @ApiPropertyOptional({ enum: PostType, default: PostType.GENERAL })
+  @IsEnum(PostType)
+  @IsOptional()
+  type?: PostType = PostType.GENERAL;
 
-//   @IsArray()
-//   @IsOptional()
-//   tags?: string[];
+  @ApiPropertyOptional({ type: [String] })
+  @IsArray()
+  @IsOptional()
+  tags?: string[] = [];
 
-//   @IsArray()
-//   @IsOptional()
-//   attachments?: string[];
-// }
+  @ApiPropertyOptional({ type: [String] })
+  @IsArray()
+  @IsOptional()
+  attachments?: string[] = [];
+}
--- src/modules/community/dto/update-post.dto.ts
+++ src/modules/community/dto/update-post.dto.ts
@@ -1,19 +1,4 @@
-// import { IsString, IsOptional, IsArray, IsBoolean } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreatePostDto } from './create-post.dto';
 
-// export class UpdatePostDto {
-//   @IsString()
-//   @IsOptional()
-//   content?: string;
-
-//   @IsArray()
-//   @IsOptional()
-//   media?: string[];
-
-//   @IsBoolean()
-//   @IsOptional()
-//   isPublic?: boolean;
-
-//   @IsArray()
-//   @IsOptional()
-//   tags?: string[];
-// }
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
